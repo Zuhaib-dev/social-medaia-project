7faabf906ff963af667fa17a751c3e4386bfba6b
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-05 19:07:13 +0530

    donw till 1:06

--- src/middlewares/auth.midlleware.js
+++ src/middlewares/auth.midlleware.js
@@ -13,6 +13,8 @@ async function authMiddleware(req, res, next) {
     const user = await userModel.findOne({
       _id: jwt.decoded.id,
     });
+    req.user = user
+    next()
   } catch (error) {
     return res.status(400).json({
       message: "invalid token , please login first ",
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -2,6 +2,13 @@ const express = require("express");
 const authMiddleware = require("../middlewares/auth.midlleware");
 const cookieParser = require("cookie-parser");
 const userModel = require("../models/user.model");
+const multer = require("multer");
 const router = express.Router();
-router.post("/", authMiddleware);
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
+router.post("/", authMiddleware,
+    upload.single("image"),
+     createPostController
+    );
 module.exports = router;
