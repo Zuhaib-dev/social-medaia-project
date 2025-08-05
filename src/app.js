88cf171d28cd4b27bbef8fb12bff53c0b1114837
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-05 18:35:16 +0530

    done till 33

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require("express")
 const authRouter = require('./routes/auth.routes')
+const postRouter = require('../src/routes/post.routes')
 const userModel = require('./models/user.model')
 const cookieParser = require('cookie-parser')
 const app = express()
 app.use(express.json())
 app.use('/api/auth', authRouter)
+app.use('/api/posts',postRouter)
 app.use(cookieParser())
 module.exports = app
