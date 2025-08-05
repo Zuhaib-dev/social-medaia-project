1b50b50432e956308f232d0f29dddf318ab76564
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-05 15:54:26 +0530

    done till bcrypt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 async function registerController(req, res) {
   const { username, password } = req.body;
   const isUserAlreadyExists = await userModel.findOne({ username });
@@ -8,40 +9,41 @@ async function registerController(req, res) {
   }
   const user = await userModel.create({
     username,
-    password,
+    password: await bcrypt.hash(password, 10),
   });
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
   res.cookie = ("token", token);
 }
-async function loginController(req,res) {
-    const {username,password} = req.body
-    const user = await userModel.findOne({username})
-    if(!user){
-        return res.status(400).json({
-            message:"user not found"
-        })
-    }
-    const isPasswordvalid = user.password === password
-    if(!isPasswordvalid){
-        return res.status(400).json({
-            message:"invalid password"
-        })
-    }
-    const token = jwt.sign({
-        id: user._id
-    },process.env.JWT_SECRET)
-    res.cookie = ('token', token)
-    res.status(200).json({
-        message:"USer logged in ",
-        user:{
-            username :user.username,
-            id:user._id
-
-        }
-    })
-
+async function loginController(req, res) {
+  const { username, password } = req.body;
+  const user = await userModel.findOne({ username });
+  if (!user) {
+    return res.status(400).json({
+      message: "user not found",
+    });
+  }
+  const isPasswordvalid = bcrypt.compare(password,user.password);
+  if (!isPasswordvalid) {
+    return res.status(400).json({
+      message: "invalid password",
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET
+  );
+  res.cookie = ("token", token);
+  res.status(200).json({
+    message: "USer logged in ",
+    user: {
+      username: user.username,
+      id: user._id,
+    },
+  });
 }
 module.exports = {
-    registerController,
-    loginController
-}
+  registerController,
+  loginController,
+};
